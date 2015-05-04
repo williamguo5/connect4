61e91c5db41683f2a1d8830d2a751cc1ae607dfd
Angela Yang <angela.yang95@gmail.com>
2015-05-04 14:43:39 +1000

    New implementation/ design of game using 2d array. takes input and runs game. Doesn't check if there has been 4 in a row.

---
+++ newImplementation/Game.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class Game {
+	private int lastMove[];
+	private int currentPlayer;
+	private int previousPlayer;
+	private int turnNumber;
+	private Board board;
+	
+	public Game() {
+		board = new Board();
+		turnNumber = 0;
+		currentPlayer = 0;
+		previousPlayer = 1;
+		lastMove = new int[2];
+		lastMove[0] = -1;
+		lastMove[1] = -1;
+	}
+	
+    public static void main(String[] args) {
+    	
+    	Game c4 = new Game();
+
+		c4.board.printBoard();
+    	while (!c4.gameOver()) {
+    		
+    		Scanner reader = new Scanner(System.in);
+    		int currPlayer = c4.getPlayer();		
+    		System.out.println(">>>>>>>>>>>>");
+    		
+    		
+    		//if (columnNumber < NUM_COLUMNS);
+    		boolean success = false;
+    		while(!success) {
+    			System.out.println("Player " + currPlayer + "'s turn. Choose a column: ");
+        		int columnNumber = reader.nextInt();
+    			success = c4.makeMove(columnNumber, currPlayer);
+    			c4.board.printBoard();
+    		}
+    		c4.nextTurn();
+    	}
+    	System.out.println("GAME OVER.");
+    	System.out.println("Player " + c4.previousPlayer + " wins.");
+    }
+    
+    /**
+     * Drops the token in given row for the current player
+     * If it's an invalid input it returns false
+     * @param columnNumber
+     * @param currPlayer
+     * @return
+     */
+    public boolean makeMove (int columnNumber, int currPlayer) {
+    	if(columnNumber >= 0 && columnNumber < 7 && !board.isColumnFull(columnNumber)) {
+	    	board.dropToken(columnNumber, currPlayer);
+	    	
+	    	int index = board.columnSize(columnNumber) - 1; // CHECK THIS
+	    	updateLast(index, columnNumber);
+	    	return true;
+    	} else {
+    		System.out.println("Choose another column");
+    		return false;
+    	}
+    }
+    
+    /**
+     * Updates Information for next turn
+     */
+    public void nextTurn() {
+    	turnNumber++;
+    	previousPlayer = currentPlayer;
+    	currentPlayer = turnNumber % 2;
+    }
+    
+    /**
+     * Checks is board is full or if last turn won the game
+     * @return
+     */
+    public boolean gameOver() {
+    	if(board.isFull()) {
+    		return true;
+    	} 
+    	return board.checkFour(lastMove);
+    }
+	
+    public int getPlayer() {
+    	return currentPlayer;
+    }
+ 
+	// SETS POSITION OF LAST TOKEN [ROW_INDEX, COL_INDEX]
+	private void updateLast (int pos, int colNum) {
+		lastMove[0] = pos;
+		lastMove[1] = colNum;
+	}
+}
+
