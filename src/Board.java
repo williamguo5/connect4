171bc11f1dccfced158e8f84e61c9e3e2623e9ed
z5014944 <z5014944@cse.unsw.EDU.AU>
2015-05-10 16:42:57 +1000

    changed board representation to use a 2D array. Implemented checking for when a player has won the game

--- src/Board.java
+++ src/Board.java
@@ -1,43 +1,155 @@
-import java.util.*;
 
 public class Board {
-
-	public final int NUM_ROWS = 6;
-	public final int NUM_COLUMNS = 7;
+	public static final int NUM_ROWS = 6;
+	public static final int NUM_COLS = 7;
+	public static final int EMPTY = -1;
 	
-	private ArrayList<Column> board;
+	private int[][] board;
 	
+	// initialise board to be EMPTY
 	public Board() {
-		board = new ArrayList<Column>();
-		for (int i = 0; i < NUM_COLUMNS; i++) {
-			board.add(new Column());
+		board = new int[NUM_ROWS][NUM_COLS];
+		for(int i = 0; i < NUM_ROWS; i++) {
+			for(int j = 0; j < NUM_COLS; j++) {
+				board[i][j] = EMPTY;
+			}
 		}
 	}
 	
-	public Column getColumn(int colNum) {
-		return board.get(colNum);
+	/**
+	 * Checks if a column is full. If the last element in the column (top of the column) 
+	 * is not empty, then it is full.
+	 * 
+	 * @param colNum
+	 * @return
+	 */
+	public boolean isColumnFull(int colNum) {
+		if (board[NUM_ROWS - 1][colNum] == EMPTY) {
+			return false;
+		}
+		return true;
+	}
+
+	
+	/**
+	 * Returns the size of a column, i.e. the number of tokens it contains
+	 * 
+	 * @param colNum
+	 * @return
+	 */
+	public int getColumnSize(int colNum) {
+		int size = 0;
+		for (int i = 0; i < NUM_ROWS; i++) {
+			if (board[i][colNum] == EMPTY) {
+				break;
+			} else {
+				size++;
+			}
+		}
+		return size;
 	}
 	
+	// DROP COUNTER. NEED TO UPDATE LAST MOVE
+	// Doesn't account for if column is full
+	public void dropToken(int colNum, int id) {
+		int pos = getColumnSize(colNum);
+		board[pos][colNum] = id;
+	}
+	
+	/**
+	 * Checks if the board is full
+	 * 
+	 * @return
+	 */
 	public boolean isFull() {
-		for (Column col : board) {
-			if (!col.isFull()) {
+		for (int i= 0; i < NUM_COLS; i++) {
+			if (!isColumnFull(i)) {
 				return false;
 			}
 		}
 		return true;
 	}
+	
+	/**
+	 * Checks for if a player has won the game
+	 * 
+	 * @param lastMove
+	 * @param player
+	 * @return
+	 */
+	public boolean checkFour(int[] lastMove, int player) {
+		int currentRow = lastMove[0];
+		int currentColumn = lastMove[1];
+		
+		int startRow = currentRow - 3;
+		if (startRow < 0) startRow = 0;
+		int endRow = currentRow + 3;
+		if (endRow > NUM_ROWS - 1) endRow = NUM_ROWS - 1;
+		
+		int startColumn = currentColumn - 3;
+		if (startColumn < 0) startColumn = 0;
+		int endColumn = currentColumn + 3;
+		if (endColumn > NUM_COLS - 1) endColumn = NUM_COLS - 1;
 
-	public void printBoard() {
-		for (int i = NUM_ROWS -1 ; i >= 0; i--) {
-			for (int j = 0; j < NUM_COLUMNS; j++) {
-				if (board.get(j).getColumn().size() <= i) {
-					System.out.print(" O");
-				} else {
-					System.out.print(" X");
-				}
-				
+		//Check horizontal
+		for (int i = startColumn, count = 0; i <= endColumn; i++) {
+			if (board[currentRow][i] == player) {
+				count++;
+			} else {
+				count = 0;
 			}
-			System.out.println();
+			if (count >= 4) return true;
 		}
+		
+		//Check vertical
+		for (int i = startRow, count = 0; i <= endRow; i++) {
+			if (board[i][currentRow] == player) {
+				count++;
+			} else {
+				count = 0;
+			}
+			if (count >= 4) return true;
+		}
+		
+		//Check diagonal with positive gradient
+		for (int i = startRow, j = startColumn, count = 0; i <= endRow; i++, j++) {
+			if (board[i][j] == player) {
+				count++;
+			} else {
+				count = 0;
+			}
+			if (count >= 4) return true;
+		}
+		
+		//Check diagonal with negative gradient
+		for (int i = endRow, j = startColumn, count = 0; i >= startRow; i--, j++) {
+			if (board[i][j] == player) {
+				count++;
+			} else {
+				count = 0;
+			}
+			if (count >= 4) return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Prints the contents of the board
+	 */
+	public void printBoard() {
+		for(int i = NUM_ROWS - 1; i >= 0; i--) {
+			for(int j = 0; j < NUM_COLS; j++) {
+				 if (board[i][j] == EMPTY) {
+					 System.out.print("|_");
+					 //Replace 0 with PlayerID constant field?
+				 } else if (board[i][j] == 0){
+					 System.out.print("|O");
+				 } else {
+					 System.out.print("|X");
+				 }
+			}
+			System.out.println("|");
+		}
+		System.out.println();
 	}
 }
--- src/Connect4.java
+++ src/Connect4.java
@@ -1,54 +1,98 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Connect4 {
-
+	private int lastMove[];
+	private int currentPlayer;
+	private int previousPlayer;
 	private int turnNumber;
-	private int player;
+	private Board board;
 	
-    private Board board;
-
-    public Connect4() {
-    	board = new Board();
-    }
-
+	public Connect4() {
+		board = new Board();
+		turnNumber = 0;
+		currentPlayer = 0;
+		previousPlayer = 1;
+		lastMove = new int[2];
+		lastMove[0] = Board.EMPTY;
+		lastMove[1] = Board.EMPTY;
+	}
+	
     public static void main(String[] args) {
     	
     	Connect4 c4 = new Connect4();
-    	int i = 0;
+
+		c4.board.printBoard();
     	while (!c4.gameOver()) {
-    		c4.board.printBoard();
-    		c4.board.getColumn(0).addToken(1);
-    		c4.board.getColumn(1).addToken(1);
-    		c4.board.getColumn(2).addToken(1);
-    		c4.board.getColumn(3).addToken(1);
-    		c4.board.getColumn(4).addToken(1);
-    		c4.board.getColumn(5).addToken(1);
-    		c4.board.getColumn(6).addToken(1);
-    		i++;
-    		//if (i == 2) break;
-    		c4.board.printBoard();
-    		System.out.println(">>>>>>");
-    	}
-    	
-    }
-    
-    public int getTurn() {
-    	return turnNumber;
-    }
-    
-    public int getPlayer() {
-    	return player;
-    }
-    
-    public Board getBoard() {
-    	return board;
-    }
-    
-    public boolean gameOver() {
-    	if (board.isFull()) {
     		
+    		Scanner reader = new Scanner(System.in);
+    		int currPlayer = c4.getPlayer();		
+    		System.out.println(">>>>>>>>>>>>");
+    		
+    		boolean success = false;
+    		
+    		while (!success) {
+    			System.out.println("Player " + currPlayer + "'s turn. Choose a column: ");
+        		int columnNumber = reader.nextInt();
+    			success = c4.makeMove(columnNumber, currPlayer);
+    			c4.board.printBoard();
+    		}
+    		c4.nextTurn();
+    	}
+    	System.out.println("GAME OVER.");
+    	System.out.println("Player " + c4.previousPlayer + " wins.");
+    }
+    
+    /**
+     * Drops the token in given row for the current player
+     * If it's an invalid input it returns false
+     * @param columnNumber
+     * @param currPlayer
+     * @return
+     */
+    public boolean makeMove (int columnNumber, int currPlayer) {
+    	if(columnNumber >= 0 && columnNumber < 7 && !board.isColumnFull(columnNumber)) {
+	    	board.dropToken(columnNumber, currPlayer);
+	    	
+	    	int index = board.getColumnSize(columnNumber) - 1; // CHECK THIS
+	    	updateLast(index, columnNumber);
+	    	return true;
+    	} else {
+    		System.out.println("Invalid move. Choose another column: ");
+    		return false;
+    	}
+    }
+    
+    /**
+     * Increments the turn number and updates the previous and current player
+     */
+    public void nextTurn() {
+    	turnNumber++;
+    	previousPlayer = currentPlayer;
+    	currentPlayer = turnNumber % 2;
+    }
+    
+    /**
+     * Checks is board is full or if the previous turn won the game
+     * @return
+     */
+    public boolean gameOver() {
+    	if(board.isFull()) {
     		return true;
+    	} 
+    	if (turnNumber!= 0) {
+    		return board.checkFour(lastMove, previousPlayer);
     	}
     	return false;
     }
+	
+    public int getPlayer() {
+    	return currentPlayer;
+    }
+ 
+	// SETS POSITION OF LAST TOKEN [ROW_INDEX, COL_INDEX]
+	private void updateLast (int rowNum, int colNum) {
+		lastMove[0] = rowNum;
+		lastMove[1] = colNum;
+	}
 }
+
