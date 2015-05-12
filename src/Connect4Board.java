4a00ea534345a745595afca311e64e45ac1b3460
William Guo <williamg2103@gmail.com>
2015-05-12 11:46:51 +1000

    Changed Connect4Board so that it is no longer a concrete solution.

--- src/Connect4Board.java
+++ src/Connect4Board.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
@@ -29,13 +30,7 @@ public class Connect4Board extends JLayeredPane implements ActionListener {
 	private String colBlank;
 	private String colMouseOver;
 	private String colClick;
-	private ColumnButton col0Button;
-	private ColumnButton col1Button;
-	private ColumnButton col2Button;
-	private ColumnButton col3Button;
-	private ColumnButton col4Button;
-	private ColumnButton col5Button;
-	private ColumnButton col6Button;
+	private ArrayList<ColumnButton> columnButtons;
 
 	private Board board;
 	
@@ -51,16 +46,11 @@ public class Connect4Board extends JLayeredPane implements ActionListener {
         colClick = "colClick.png";
         setBackgroundImg("background2.png");
 		setLayout(new GridLayout(1, 7));
-        col0Button = new ColumnButton();
-		col1Button = new ColumnButton();
-		col2Button = new ColumnButton();
-		col3Button = new ColumnButton();
-		col4Button = new ColumnButton();
-		col5Button = new ColumnButton();
-		col6Button = new ColumnButton();
 		
-//		for(int i = 0; i < NUM_COLS;)
-		
+		columnButtons = new ArrayList<ColumnButton>();
+		for(int i = 0; i < board.NUM_COLS; i++){
+			columnButtons.add(new ColumnButton(i));
+		}
 		generateBoard();
     }
 
@@ -69,460 +59,71 @@ public class Connect4Board extends JLayeredPane implements ActionListener {
 	 * NOTE: this isn't a real/valid soduku board.
 	 * @throws IOException 
 	 */
-    public void generateBoard() throws IOException {
-    	add(col0Button);
-		add(col1Button);
-		add(col2Button);
-		add(col3Button);
-		add(col4Button);
-		add(col5Button);
-		add(col6Button);
+    public void generateBoard() throws IOException {    	
+    	for(int i = 0; i < board.NUM_COLS; i++){
+    		add(columnButtons.get(i));
+    		columnButtons.get(i).setBackgroundImg(colBlank);
+    		final int colNum = i;
+	    	columnButtons.get(i).addMouseListener(new MouseListener() {
+	    		@Override
+		        public void mouseReleased(MouseEvent e) {
+	    			try {
+	    	      		columnButtons.get(colNum).setBackgroundImg(colMouseOver);
+	    	      		columnButtons.get(colNum).repaint();
+	    			} catch (IOException e1){
+	    				e1.printStackTrace();
+	    			}
+		        }
+		        @Override
+		        public void mousePressed(MouseEvent e) {
+		        	dropToken(colNum);
+		    		try {
+		    			columnButtons.get(colNum).setBackgroundImg(colClick);
+		    			columnButtons.get(colNum).repaint();
+		    		} catch (IOException e1) {
+		    			e1.printStackTrace();
+		    		}
+		        }
+		        @Override
+		        public void mouseClicked(MouseEvent e) {
 		
-		col0Button.setBackgroundImg(colBlank);
-		col1Button.setBackgroundImg(colBlank);
-		col2Button.setBackgroundImg(colBlank);
-		col3Button.setBackgroundImg(colBlank);
-		col4Button.setBackgroundImg(colBlank);
-		col5Button.setBackgroundImg(colBlank);
-		col6Button.setBackgroundImg(colBlank);
-		
-		createListeners();
+		        }
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					try {
+						columnButtons.get(colNum).setBackgroundImg(colMouseOver);
+						columnButtons.get(colNum).repaint();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+				@Override
+				public void mouseExited(MouseEvent e) {
+					try {
+						columnButtons.get(colNum).setBackgroundImg(colBlank);
+						columnButtons.get(colNum).repaint();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			});
+    	}
 	}
     
-    
-    public void createListeners(){
-    	col0Button.addMouseListener(new MouseListener() {
-          @Override
-          public void mouseReleased(MouseEvent e) {
-              try {
-					col0Button.setBackgroundImg(colMouseOver);
-					col0Button.repaint();
-				} catch (IOException e1){
-					
-					e1.printStackTrace();
-				}
-          }
-          @Override
-          public void mousePressed(MouseEvent e) {
-//        	  if(!col0Button.isFull()){
-//        		  col0Button.addToken(getPlayerID());
-//        	  }
-        	  if(board.isColumnFull(0)){
-        		  System.out.println("COL FULL");
-        		  return;
-        	  }
-        	  if(board.dropToken(0, board.getCurrentPlayer())){
-        		  col0Button.addToken(board.getCurrentPlayer());
-        	  }
-        	  
-        	  
-				try {
-					col0Button.setBackgroundImg(colClick);
-					col0Button.repaint();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-          }
-          @Override
-          public void mouseClicked(MouseEvent e) {
-//          	try {
-//					col0Button.setBackgroundImg("colBlank.png");
-//					col0Button.repaint();
-//					connect4Board.repaint();
-//				} catch (IOException e1) {
-//					e1.printStackTrace();
-//				}
-          }
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				try {
-					col0Button.setBackgroundImg(colMouseOver);
-					col0Button.repaint();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			}
-			@Override
-			public void mouseExited(MouseEvent e) {
-				try {
-					col0Button.setBackgroundImg(colBlank);
-					col0Button.repaint();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			}
-		});
-    	
-    	col1Button.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                try {
-  					col1Button.setBackgroundImg(colMouseOver);
-  					col1Button.repaint();
-  				} catch (IOException e1) {
-  					
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mousePressed(MouseEvent e) {
-            	if(board.isColumnFull(1)){
-          		  System.out.println("COL FULL");
-          		  return;
-          	  	}
-          	  	if(board.dropToken(1, board.getCurrentPlayer())){
-          	  		col1Button.addToken(board.getCurrentPlayer());
-            	}
-            	try {
-  					col1Button.setBackgroundImg(colClick);
-  					col1Button.repaint();
-  				} catch (IOException e1) {
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mouseClicked(MouseEvent e) {
-//            	try {
-//  					col0Button.setBackgroundImg("colBlank.png");
-//  					col0Button.repaint();
-//  					connect4Board.repaint();
-//  				} catch (IOException e1) {
-//  					e1.printStackTrace();
-//  				}
-            }
-  			@Override
-  			public void mouseEntered(MouseEvent e) {
-  				try {
-  					col1Button.setBackgroundImg(colMouseOver);
-  					col1Button.repaint();
-  				} catch (IOException e1) {
-  					e1.printStackTrace();
-  				}
-  			}
-  			@Override
-  			public void mouseExited(MouseEvent e) {
-  				try {
-  					col1Button.setBackgroundImg(colBlank);
-  					col1Button.repaint();
-  				} catch (IOException e1) {
-  					e1.printStackTrace();
-  				}
-  			}
-  		});
-    	
-    	col2Button.addMouseListener(new MouseListener() {
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                try {
-  					col2Button.setBackgroundImg(colMouseOver);
-  					col2Button.repaint();
-  				} catch (IOException e1) {
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mousePressed(MouseEvent e) {
-            	if(board.isColumnFull(2)){
-            		System.out.println("COL FULL");
-	          		return;
-	          	}
-	          	if(board.dropToken(2, board.getCurrentPlayer())){
-	          		col2Button.addToken(board.getCurrentPlayer());
-            	}
-                try {
-  					col2Button.setBackgroundImg(colClick);
-  					col2Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mouseClicked(MouseEvent e) {
-//            	try {
-//  					col0Button.setBackgroundImg("colBlank.png");
-//  					col0Button.repaint();
-//  					connect4Board.repaint();
-//  				} catch (IOException e1) {
-
-//  					e1.printStackTrace();
-//  				}
-            }
-  			@Override
-  			public void mouseEntered(MouseEvent e) {
-  				try {
-  					col2Button.setBackgroundImg(colMouseOver);
-  					col2Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-  			}
-  			@Override
-  			public void mouseExited(MouseEvent e) {
-  				try {
-  					col2Button.setBackgroundImg(colBlank);
-  					col2Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-  			}
-  		});
-    
-    	col3Button.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                try {
-  					col3Button.setBackgroundImg(colMouseOver);
-  					col3Button.repaint();
-  				} catch (IOException e1) {
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mousePressed(MouseEvent e) {
-            	if(board.isColumnFull(3)){
-            		System.out.println("COL FULL");
-	          		return;
-	          	}
-	          	if(board.dropToken(3, board.getCurrentPlayer())){
-	          		col3Button.addToken(board.getCurrentPlayer());
-	          	}
-                try {
-  					col3Button.setBackgroundImg(colClick);
-  					col3Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mouseClicked(MouseEvent e) {
-//            	try {
-//  					col0Button.setBackgroundImg("colBlank.png");
-//  					col0Button.repaint();
-//  					connect4Board.repaint();
-//  				} catch (IOException e1) {
-
-//  					e1.printStackTrace();
-//  				}
-            }
-  			@Override
-  			public void mouseEntered(MouseEvent e) {
-  				try {
-  					col3Button.setBackgroundImg(colMouseOver);
-  					col3Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-  			}
-  			@Override
-  			public void mouseExited(MouseEvent e) {
-  				try {
-  					col3Button.setBackgroundImg(colBlank);
-  					col3Button.repaint();
-  				} catch (IOException e1) {
-  					e1.printStackTrace();
-  				}
-  			}
-  		});
-    
-    	col4Button.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                try {
-  					col4Button.setBackgroundImg(colMouseOver);
-  					col4Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mousePressed(MouseEvent e) {
-            	if(board.isColumnFull(4)){
-            		System.out.println("COL FULL");
-	          		return;
-	          	}
-	          	if(board.dropToken(4, board.getCurrentPlayer())){
-	          		col4Button.addToken(board.getCurrentPlayer());
-	          	}
-                try {
-  					col4Button.setBackgroundImg(colClick);
-  					col4Button.repaint();
-  				} catch (IOException e1) {
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mouseClicked(MouseEvent e) {
-//            	try {
-//  					col0Button.setBackgroundImg("colBlank.png");
-//  					col0Button.repaint();
-//  					connect4Board.repaint();
-//  				} catch (IOException e1) {
-
-//  					e1.printStackTrace();
-//  				}
-            }
-  			@Override
-  			public void mouseEntered(MouseEvent e) {
-  				try {
-  					col4Button.setBackgroundImg(colMouseOver);
-  					col4Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-  			}
-  			@Override
-  			public void mouseExited(MouseEvent e) {
-  				try {
-  					col4Button.setBackgroundImg(colBlank);
-  					col4Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-  			}
-  		});
-    	
-    	col5Button.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                try {
-  					col5Button.setBackgroundImg(colMouseOver);
-  					col5Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mousePressed(MouseEvent e) {
-            	if(board.isColumnFull(5)){
-            		System.out.println("COL FULL");
-	          		return;
-	          	}
-	          	if(board.dropToken(5, board.getCurrentPlayer())){
-	          		col5Button.addToken(board.getCurrentPlayer());
-	          	}
-                try {
-  					col5Button.setBackgroundImg(colClick);
-  					col5Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mouseClicked(MouseEvent e) {
-//            	try {
-//  					col0Button.setBackgroundImg("colBlank.png");
-//  					col0Button.repaint();
-//  					connect4Board.repaint();
-//  				} catch (IOException e1) {
-
-//  					e1.printStackTrace();
-//  				}
-            }
-  			@Override
-  			public void mouseEntered(MouseEvent e) {
-  				try {
-  					col5Button.setBackgroundImg(colMouseOver);
-  					col5Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-  			}
-  			@Override
-  			public void mouseExited(MouseEvent e) {
-  				try {
-  					col5Button.setBackgroundImg(colBlank);
-  					col5Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-  			}
-  		});
-   
-    	col6Button.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                try {
-  					col6Button.setBackgroundImg(colMouseOver);
-  					col6Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mousePressed(MouseEvent e) {
-            	if(board.isColumnFull(6)){
-            		System.out.println("COL FULL");
-	          		return;
-	          	}
-	          	if(board.dropToken(6, board.getCurrentPlayer())){
-	          		col6Button.addToken(board.getCurrentPlayer());
-	          	}
-                try {
-  					col6Button.setBackgroundImg(colClick);
-  					col6Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-            }
-            @Override
-            public void mouseClicked(MouseEvent e) {
-//            	try {
-//  					col0Button.setBackgroundImg("colBlank.png");
-//  					col0Button.repaint();
-//  					connect4Board.repaint();
-//  				} catch (IOException e1) {
-
-//  					e1.printStackTrace();
-//  				}
-            }
-  			@Override
-  			public void mouseEntered(MouseEvent e) {
-  				try {
-  					col6Button.setBackgroundImg(colMouseOver);
-  					col6Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-  			}
-  			@Override
-  			public void mouseExited(MouseEvent e) {
-  				try {
-  					col6Button.setBackgroundImg(colBlank);
-  					col6Button.repaint();
-  				} catch (IOException e1) {
-
-  					e1.printStackTrace();
-  				}
-  			}
-  		});
-    
-    
+    public void dropToken(int colNum){
+    	if(board.isColumnFull(colNum)){
+  		  	System.out.println("COL FULL");
+  		  	return;
+  	  	}
+	  	if(board.dropToken(colNum, board.getCurrentPlayer())){
+	  		columnButtons.get(colNum).addToken(board.getCurrentPlayer());
+	  	}  
     }
     
     public void clearBoard(){
-    	col0Button.resetTokens();
-    	col1Button.resetTokens();
-    	col2Button.resetTokens();
-    	col3Button.resetTokens();
-    	col4Button.resetTokens();
-    	col5Button.resetTokens();
-    	col6Button.resetTokens();
+    	for(int i = 0; i < board.NUM_COLS; i++){
+    		columnButtons.get(i).resetTokens();
+    	}
     }
 
     /**
