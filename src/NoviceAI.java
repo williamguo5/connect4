aa710e7ae8d269018af3170914db21fc2aa3a306
Jeff <jeff@laptop-a.(none)>
2015-05-20 11:45:20 +1000

    Minor changes to NoviceAI

--- src/NoviceAI.java
+++ src/NoviceAI.java
@@ -2,41 +2,37 @@
 public class NoviceAI implements Player {
 	public int getMove(Board state) {
 		int move = 0;
-
-		//This really basic AI will only try to block 4 in a rows across or down, but only tries to find out via scanning the last move made
-		//It will not try to block a win via a diagonal 4 in a row
-		//If it cannot find any it will simply find the first empty column
-		//to place its token in
+		int lastMove[] = state.getLastMove();
 		int me = state.getCurrentPlayer();
-		int opponent = -1;
-		if (me == 1) { //one player is 0 and another is 1
-			opponent = 0;
-		} else {
-			opponent = 1;
-		}
-		if (state.isGameOver()) {
-			return 1;
-		}
-		if (state.getTurnNumber() > 0) {
-			int lastMove[] = state.getLastMove();
-			//Check the row across where last move was made for 3 in a row
+		int opponent = state.getPreviousPlayer();
+		/*if (state.getTurnNumber() > 0) {
+			
+			boolean block = false;
 			for (int i = 0, count = 0; i < Board.NUM_COLS; i++) {
 				if (state.getBoard()[lastMove[0]][i] == opponent) {
 					count++;
-					System.out.print("Hi");
 				} else {
 					count = 0;
-					System.out.print("Bye");
 				}
-				if (count == 3 && i < Board.NUM_COLS && !state.isColumnFull(i+1)) {
-					move = i+1; //block horizontal win
-				} else if (count == 3 && i == Board.NUM_COLS && state.isColumnFull(i-3)) {
-					move = i-3; //|_|_|_|here|x|x|x|
-				} else {
-					move = -1;
+				
+				if (count == 2 && i < Board.NUM_COLS - 1 && !state.isColumnFull(i+1)) {
+					//return i+1;
+					move = i+1;
+					block = true;
+					//break;
+				} else if (count == 2 && i == Board.NUM_COLS - 1 && state.isColumnFull(i-2)) {
+					//return i-2;
+					move = i-2;
+					block = true;
+					//break;
+				} else if (count == 3 && i < Board.NUM_COLS && !state.isColumnFull(i+1)) {
+					
 				}
+				System.out.println("Move is " + move);
 			}
-
+			if (!block) {
+				move = -1;
+			}*/
 			//Check the column where last move was made for 3 in a row
 			for (int i = 0, count = 0; i < Board.NUM_ROWS; i++) {
 				if (state.isColumnFull(lastMove[1])) break;
@@ -47,21 +43,28 @@ public class NoviceAI implements Player {
 				}
 				if (count == 3)  {
 					move = lastMove[1]; //block off the vertical win
+					break;
 				} else {
 					move = -1;
 				}
 			}
-		}
+		//}
 		for (int col = 0; col < Board.NUM_COLS; col++) {
 			if (!state.isColumnFull(col)) {
-				state.setAI(0); //prevents an ai move from being called when simulating game states
+				move = col;
 				state.simDropToken(col);
 				if (state.isGameOver()) {
 					move = col; //instantly place the token that will win the game
 					state.undoPreviousMove();
 					return move;
-				} else if (move == -1) {
-					return col;
+				} else {				
+					for (int i = 0; i < Board.NUM_COLS; i++) {
+						Board opponentTurn = state.clone();
+						opponentTurn.simDropToken(i);
+						if (opponentTurn.isGameOver()) {
+							return i;
+						}
+					}
 				}
 			}
 		}
