946131398d67e4f0f5ef51f39f43550518487f50
Jeff <jeff@laptop-a.(none)>
2015-05-21 15:45:37 +1000

    small improvement on NoviceAI

--- src/NoviceAI.java
+++ src/NoviceAI.java
@@ -3,63 +3,30 @@ public class NoviceAI implements Player {
 	public int getMove(Board state) {
 		int move = 0;
 		int lastMove[] = state.getLastMove();
-		int me = state.getCurrentPlayer();
 		int opponent = state.getPreviousPlayer();
-		/*if (state.getTurnNumber() > 0) {
-			
-			boolean block = false;
-			for (int i = 0, count = 0; i < Board.NUM_COLS; i++) {
-				if (state.getBoard()[lastMove[0]][i] == opponent) {
-					count++;
-				} else {
-					count = 0;
-				}
-				
-				if (count == 2 && i < Board.NUM_COLS - 1 && !state.isColumnFull(i+1)) {
-					//return i+1;
-					move = i+1;
-					block = true;
-					//break;
-				} else if (count == 2 && i == Board.NUM_COLS - 1 && state.isColumnFull(i-2)) {
-					//return i-2;
-					move = i-2;
-					block = true;
-					//break;
-				} else if (count == 3 && i < Board.NUM_COLS && !state.isColumnFull(i+1)) {
-					
-				}
-				System.out.println("Move is " + move);
+		for (int i = 0, count = 0; i < Board.NUM_ROWS; i++) {
+			if (state.isColumnFull(lastMove[1])) break;
+			if (state.getBoard()[i][lastMove[1]] == opponent) {
+				count++;
+			} else {
+				count = 0;
 			}
-			if (!block) {
-				move = -1;
-			}*/
-			//Check the column where last move was made for 3 in a row
-			for (int i = 0, count = 0; i < Board.NUM_ROWS; i++) {
-				if (state.isColumnFull(lastMove[1])) break;
-				if (state.getBoard()[i][lastMove[1]] == opponent) {
-					count++;
-				} else {
-					count = 0;
-				}
-				if (count == 3)  {
-					move = lastMove[1]; //block off the vertical win
-					break;
-				} else {
-					move = -1;
-				}
+			if (count == 3)  {
+				move = lastMove[1]; //block off the vertical win
+				break;
 			}
-		//}
+		}
 		for (int col = 0; col < Board.NUM_COLS; col++) {
 			if (!state.isColumnFull(col)) {
+				Board myMove = state.clone();
 				move = col;
-				state.simDropToken(col);
-				if (state.isGameOver()) {
+				myMove.simDropToken(col);
+				if (myMove.isGameOver()) {
 					move = col; //instantly place the token that will win the game
-					state.undoPreviousMove();
 					return move;
 				} else {				
 					for (int i = 0; i < Board.NUM_COLS; i++) {
-						Board opponentTurn = state.clone();
+						Board opponentTurn = myMove.clone();
 						opponentTurn.simDropToken(i);
 						if (opponentTurn.isGameOver()) {
 							return i;
@@ -68,6 +35,23 @@ public class NoviceAI implements Player {
 				}
 			}
 		}
-        return move;
+		int start = 0;
+		for (int j = 0; j < 4; j++) {
+			for (int i = start, count = 0; i < start + 4; i++) {
+				for (int k = 0; k < Board.NUM_ROWS; k++) {
+					if (state.getBoard()[k][i] == opponent) {
+						count++;
+					}
+					if (count == 2 && i != Board.NUM_COLS - 1 && !state.isColumnFull(i)) {
+						if (!state.isColumnFull(i+1)) return i+1;
+					} else if (count == 2 && i == Board.NUM_COLS -1 && state.isColumnFull(i)){
+						if (!state.isColumnFull(i-2)) return i-2;
+					}
+				}
+				
+			}
+			start++;
+		}
+		return move;
     }
 }
